b71f0b9f1b6c41e9da2c24b6ebaaebc6176e5f98
Dror Gluska <drorgl@yahoo.com>
2017-09-27 20:31:46 +0300

    attempt to support non-standard c++ extension

--- build_windows.bat
+++ build_windows.bat
@@ -1,3 +1,3 @@
-gyp capnproto.gyp -DOS=win -Dtarget_arch=x64 --depth=. -f msvs -G msvs_version=2017 -Dbuildtype=Debug --no-duplicate-basename-check --generator-output=./build.vs2017/
+gyp capnproto.gyp -DOS=win -Dtarget_arch=x64 --depth=.  -f msvs -G msvs_version=2017 -Dbuildtype=Debug --no-duplicate-basename-check --generator-output=./build.vs2017/
 
 rem msbuild.exe ......SolutionFile.sln /t:Build/p:Configuration=Release;Platform=Win32
